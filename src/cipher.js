e9c1ce3b9bba93e355daa5e58c267f856a1c9e0d
ericaserpa <ericaserpa@hotmail.com>
2021-06-07 15:13:55 -0300

    primeiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,23 @@
-const cipher = {
+//const cipher = {
   // ...
-};
+//};
 
-export default cipher;
+//export default cipher;
+/*Function cipher (string)
+
+    const cipher = (string, num) =>
+    let cipher = "", code;
+    for (let i=0; i<string.length;i++){
+        if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+            code = (((string.charCodeAt(i)-65)+n)%26)+65;
+        }
+        else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+        }
+        else if (string.charCodeAt(i)===32){
+            code = 32;
+        }
+        result+=String.fromCharCode(code);
+      
+        {
+          return result; ""
+    
