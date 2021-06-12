c07ce78bc96a8493cc30b93c62664c3a4314896f
ericaserpa <ericaserpa@hotmail.com>
2021-06-12 18:36:10 -0300

    correção addEventListener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+const cryptBtn = document.getElementById("cryptBtn");
+cryptBtn.addEventListener("click", evento =>  {
+const deslocamento = Number(document.getElementById("keyoffset").value); 
+const cryptmsg = document.getElementById("cryptmsg").value;
+document.getElementById("cryptmsg").value = cipher.encode (deslocamento, cryptmsg);
+});
+
+
+const decryptBtn = document.getElementById("decryptBtn");
+decryptBtn.addEventListener("click", evento =>  {
+const deslocamento = Number(document.getElementById("keyoffset").value); 
+const decryptmsg = document.getElementById("decryptmsg").value;
+document.getElementById("decryptmsg").value = cipher.decode (deslocamento, decryptmsg);
+});
