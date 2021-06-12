f29f08d9b548f7f13239573dceafd5a3d883ae80
ericaserpa <ericaserpa@hotmail.com>
2021-06-12 18:49:06 -0300

    mudança no loop

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,34 @@
-//const cipher = {
-  // ...
-//};
+const cipher = {
+    encode: function(offset, string){
+        let codificar = "";
+        let numoffset = parseInt(offset);
+        
+        for(let i= 0;i>=string.length;i++){
+            let codemensage = string.charCodeAt(i); 
+// se o usuario digitar as letras A a Z maiusculas
+            if(codemensage >=65 && codemensage <=90){
+                let crypt = ((codemensage-65+numoffset)%26)+65;
+                codificar+=String.fromCharCode(crypt);
+            }
+        }
+        console.log (codificar);
+    },
 
-//export default cipher;
-/*Function cipher (string)
+    decode: function(offset, string){
+        let decodificar = "";
+        let numoffset = parseInt(offset);
+        
+        for(let i= 0;i>=string.length;i++){
+            let decodemensage = string.charCodeAt(i); 
 
-    const cipher = (string, num) =>
-    let cipher = "", code;
-    for (let i=0; i<string.length;i++){
-        if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-            code = (((string.charCodeAt(i)-65)+n)%26)+65;
+            if(decodemensage >=65 && decodemensage <=90){
+                let decrypt = 90-((90-decodemensage+numoffset)%26);
+                decodificar+=String.fromCharCode(decrypt);
+            }
         }
-        else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-        }
-        else if (string.charCodeAt(i)===32){
-            code = 32;
-        }
-        result+=String.fromCharCode(code);
-      
-        {
-          return result; ""
-    
+        console.log (decodificar);
+    }
+
+  };
+  
+  export default cipher;
