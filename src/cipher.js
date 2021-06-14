79ea12efb2fd4ea3c13b6d21571ff0dece3acbef
ericaserpa <ericaserpa@hotmail.com>
2021-06-14 18:13:08 -0300

    preventDefault inserido

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,38 @@
 const cipher = {
     encode: function(offset, string){
         let codificar = "";
-        let numoffset = parseInt(offset);
+
+        //console.log (offset,string);
         
-        for(let i= 0;i>=string.length;i++){
-            let codemensage = string.charCodeAt(i); 
+        for(let i= 0;i<string.length;i++){
+            let codeMensage = string.charCodeAt(i); 
 // se o usuario digitar as letras A a Z maiusculas
-            if(codemensage >=65 && codemensage <=90){
-                let crypt = ((codemensage-65+numoffset)%26)+65;
+            if(codeMensage >=65 && codeMensage <=90){
+                let crypt = ((codeMensage-65+offset)%26)+65;
                 codificar+=String.fromCharCode(crypt);
             }
         }
         console.log (codificar);
+        return codificar;
     },
 
     decode: function(offset, string){
         let decodificar = "";
-        let numoffset = parseInt(offset);
-        
-        for(let i= 0;i>=string.length;i++){
-            let decodemensage = string.charCodeAt(i); 
 
-            if(decodemensage >=65 && decodemensage <=90){
-                let decrypt = 90-((90-decodemensage+numoffset)%26);
+        //console.log (offset,string);
+
+        for(let i= 0;i<string.length;i++){
+            let decodeMensage = string.charCodeAt(i); 
+
+            if(decodeMensage >=65 && decodeMensage <=90){
+                let decrypt = 90-((90-decodeMensage+offset)%26);
                 decodificar+=String.fromCharCode(decrypt);
             }
         }
         console.log (decodificar);
+        return decodificar;
     }
 
-  };
+  }
   
   export default cipher;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cipher from './cipher.js';
 
 const cryptBtn = document.getElementById("cryptBtn");
 cryptBtn.addEventListener("click", evento =>  {
+evento.preventDefault();
 const deslocamento = Number(document.getElementById("keyoffset").value); 
 const cryptmsg = document.getElementById("cryptmsg").value;
 document.getElementById("cryptmsg").value = cipher.encode (deslocamento, cryptmsg);
@@ -11,6 +12,7 @@ document.getElementById("cryptmsg").value = cipher.encode (deslocamento, cryptms
 
 const decryptBtn = document.getElementById("decryptBtn");
 decryptBtn.addEventListener("click", evento =>  {
+evento.preventDefault();
 const deslocamento = Number(document.getElementById("keyoffset").value); 
 const decryptmsg = document.getElementById("decryptmsg").value;
 document.getElementById("decryptmsg").value = cipher.decode (deslocamento, decryptmsg);
